faea1cb3d932896520b8c0d9864d51b50f1f9ec6
narupo <narupo.glab@gmail.com>
2022-07-30 05:18:12 +0900

    imp listdir()

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,7 @@ use npr as r;
 
 fn main() {
     assert_eq!(r::exists("LICENSE"), true);
+
+    let files = r::listdir("sample/listdir");
+    assert_eq!(files.len(), 3);
 }
--- src/npr.rs
+++ src/npr.rs
@@ -1,5 +1,16 @@
 use std::path::{ Path };
+use std::fs;
 
 pub fn exists(fname: &str) -> bool {
     Path::new(fname).exists()
+}
+
+pub fn listdir(fname: &str) -> Vec<String> {
+    let mut v = vec![];
+
+    for file in fs::read_dir(fname).unwrap() {
+        v.push(file.unwrap().path().into_os_string().into_string().unwrap());
+    }
+
+    v
 }
